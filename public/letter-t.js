0bd4a469f0cbdc94ba85421ec6523b2f9fea7a71
DeepImpact Test <admin+ditest@deep-impact.ch>
2019-08-30 13:32:40 +0200

    Format

--- public/letter-t.js
+++ public/letter-t.js
@@ -3,19 +3,39 @@ import { html, render } from "https://unpkg.com/lit-html?module";
 const htmlTemplate = () => html`
   <style>
     @keyframes morph-horizontal-bar {
-      0%   {clip-path: polygon(50% 20%,  50% 20%,  50% 0, 50% 0);} /* centered top */
-      15%  {clip-path: polygon(50% 20%,  50% 20%,  50% 0, 50% 0);} /* centered top */
-      30%  {clip-path: polygon( 0  20%, 100% 20%, 100% 0,  0  0);} /* fully streched */
-      70%  {clip-path: polygon( 0  20%, 100% 20%, 100% 0,  0  0);} /* fully streched */
-      85%  {clip-path: polygon(50% 20%,  50% 20%,  50% 0, 50% 0);} /* centered top */
-      100% {clip-path: polygon(50% 20%,  50% 20%,  50% 0, 50% 0);} /* centered top */
+      0% {
+        clip-path: polygon(50% 20%, 50% 20%, 50% 0, 50% 0);
+      } /* centered top */
+      15% {
+        clip-path: polygon(50% 20%, 50% 20%, 50% 0, 50% 0);
+      } /* centered top */
+      30% {
+        clip-path: polygon(0 20%, 100% 20%, 100% 0, 0 0);
+      } /* fully streched */
+      70% {
+        clip-path: polygon(0 20%, 100% 20%, 100% 0, 0 0);
+      } /* fully streched */
+      85% {
+        clip-path: polygon(50% 20%, 50% 20%, 50% 0, 50% 0);
+      } /* centered top */
+      100% {
+        clip-path: polygon(50% 20%, 50% 20%, 50% 0, 50% 0);
+      } /* centered top */
     }
 
     @keyframes morph-vertical-bar {
-      0%   {clip-path: polygon(40% 100%, 60% 100%, 60% 100%, 40% 100%);} /* flat bottom */
-      15%  {clip-path: polygon(40%   0 , 60%   0 , 60% 100%, 40% 100%);} /* fully streched */
-      85%  {clip-path: polygon(40%   0 , 60%   0 , 60% 100%, 40% 100%);} /* fully streched */
-      100% {clip-path: polygon(40% 100%, 69% 100%, 60% 100%, 40% 100%);} /* flat bottom */
+      0% {
+        clip-path: polygon(40% 100%, 60% 100%, 60% 100%, 40% 100%);
+      } /* flat bottom */
+      15% {
+        clip-path: polygon(40% 0, 60% 0, 60% 100%, 40% 100%);
+      } /* fully streched */
+      85% {
+        clip-path: polygon(40% 0, 60% 0, 60% 100%, 40% 100%);
+      } /* fully streched */
+      100% {
+        clip-path: polygon(40% 100%, 69% 100%, 60% 100%, 40% 100%);
+      } /* flat bottom */
     }
 
     .letter {
@@ -39,8 +59,6 @@ const htmlTemplate = () => html`
     .vertical-bar {
       animation: morph-vertical-bar 2s infinite;
     }
-
-
   </style>
   <span class="letter">
     &nbsp;
