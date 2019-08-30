9fbff554d4dacf3e09c543af16328052a52180a3
Sam Spycher <sam.spycher@deep-impact.ch>
2019-08-30 14:26:37 +0200

    Add letter j

--- public/letter-j.js
+++ public/letter-j.js
@@ -3,6 +3,10 @@ import { html, render } from "https://unpkg.com/lit-html?module";
 const htmlTemplate = () => html`
   <style>
     .letter {
+      font-weight: 600;
+      text-decoration: underline;
+      transform: rotate(180deg);
+      color: teal;
     }
   </style>
   <span class="letter">
