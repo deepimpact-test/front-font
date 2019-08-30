09ff2a57d078b948f6c475ecb5d74e6913997b46
Marco <hopper@Marcos-MacBook-Pro-2.local>
2019-08-30 15:10:04 +0200

    added letter z

--- public/letter-z.js
+++ public/letter-z.js
@@ -3,10 +3,20 @@ import { html, render } from "https://unpkg.com/lit-html?module";
 const htmlTemplate = () => html`
   <style>
     .letter {
+      background-color: #00FFF8;
+      color: #22002B;
+      font-size: 1.6rem;
+      line-height: 3rem;
+      width: 3rem;
+      height: 3rem;
+      border-radius: 1.5rem;
+      letter-spacing: -0.2rem;
+      font-weight: bold;
+      overflow: visible !important;
     }
   </style>
   <span class="letter">
-    Z
+    Z/>
   </span>
 `;
 
